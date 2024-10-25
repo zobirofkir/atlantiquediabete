b6994f708ce4a3a5bdcffbd5cdec675c9151a479
zobirofkir <zobirofkir19@gmail.com>
2024-10-25 23:37:52 +0100

    remove toastify notification

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
     <title>{{config('app.name')}}</title>
 </head>
 <body class="bg-gray-100">
@@ -15,7 +14,6 @@
         {{ $slot }}
     </main>
 
-    <script src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
     <script src="{{asset('assets/js/main.js')}}"></script>
 </body>
 </html>
